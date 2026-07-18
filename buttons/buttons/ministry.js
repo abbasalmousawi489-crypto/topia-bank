212a6986d1fab3e1bc89d9303cf54423f737e235
abbasalmousawi489-crypto <abbasalmousawi489@gmail.com>
2026-07-19 02:00:39 +0300

    Update ministry.js

--- buttons/buttons/ministry.js
+++ buttons/buttons/ministry.js
@@ -1,7 +1,22 @@
-module.exports = {
-  customId: "ministry",
+const { EmbedBuilder } = require("discord.js");
 
-  async execute(interaction) {
-    await interaction.reply("🏛️ قائمة الوزارات");
-  }
+module.exports = async (interaction) => {
+
+    const embed = new EmbedBuilder()
+        .setTitle("🏛️ خدمات الوزارات")
+        .setDescription(
+            "يمكنك إدارة الحسابات الخاصة بالوزارات.\n\n" +
+            "📑 متابعة الميزانية\n" +
+            "💰 إدارة الأموال الحكومية\n" +
+            "🏦 الخدمات المصرفية للوزارة"
+        )
+        .setFooter({
+            text: "بنك دينار توبي"
+        });
+
+    await interaction.reply({
+        embeds: [embed],
+        ephemeral: true
+    });
+
 };
