a96f06c91eabd2482d8ff4c7ea68346a237e0bf8
abbasalmousawi489-crypto <abbasalmousawi489@gmail.com>
2026-07-19 02:12:14 +0300

    Update withdraw.js

--- buttons/buttons/withdraw.js
+++ buttons/buttons/withdraw.js
@@ -1,12 +1,33 @@
 const { EmbedBuilder } = require("discord.js");
+const database = require("../database/database");
 
 module.exports = async (interaction) => {
 
+    const id = interaction.user.id;
+
+    const مبلغ = 200;
+
+    const حساب = database.جلب_بيانات_المستخدم(id);
+
+
+    if (حساب.الرصيد < مبلغ) {
+
+        return interaction.reply({
+            content: "❌ لا يوجد لديك رصيد كافي للسحب",
+            ephemeral: true
+        });
+
+    }
+
+
+    database.خصم_رصيد(id, مبلغ);
+
+
     const embed = new EmbedBuilder()
-        .setTitle("📤 السحب")
+        .setTitle("📤 تم السحب")
         .setDescription(
-            "💸 يمكنك سحب الأموال من حسابك.\n\n" +
-            "عملة البنك: دينار توبي"
+            `✅ تم سحب ${مبلغ} دينار توبي من حسابك\n\n` +
+            `💰 العملية ناجحة`
         )
         .setFooter({
             text: "بنك دينار توبي"
