656a66b7343640490a9ba3f62e68cd912aa483d4
abbasalmousawi489-crypto <abbasalmousawi489@gmail.com>
2026-07-19 02:01:17 +0300

    Update card.js

--- buttons/buttons/card.js
+++ buttons/buttons/card.js
@@ -1,7 +1,21 @@
-module.exports = {
-  customId: "card",
+const { EmbedBuilder } = require("discord.js");
 
-  async execute(interaction) {
-    await interaction.reply("💳 معلومات البطاقة");
-  }
+module.exports = async (interaction) => {
+
+    const embed = new EmbedBuilder()
+        .setTitle("💳 البطاقة البنكية")
+        .setDescription(
+            "بطاقتك البنكية الخاصة ببنك دينار توبي.\n\n" +
+            "💳 النوع: بطاقة مواطن\n" +
+            "🟢 الحالة: فعالة"
+        )
+        .setFooter({
+            text: "بنك دينار توبي"
+        });
+
+    await interaction.reply({
+        embeds: [embed],
+        ephemeral: true
+    });
+
 };
