f019ec7d3a848ffaae36180406cec736c4735f8f
abbasalmousawi489-crypto <abbasalmousawi489@gmail.com>
2026-07-19 00:56:50 +0300

    Update accounts.js

--- database/    database/accounts.js
+++ database/    database/accounts.js
@@ -1,20 +1,76 @@
 const database = require("./database");
 
 module.exports = {
+
   create(userId) {
+
     const account = {
       id: userId,
-      money: 0,
-      card: false
+      money: 10000,
+      currency: "دينار توبي",
+      card: true
     };
 
     database.accounts.push(account);
+
     return account;
   },
 
+
   get(userId) {
-    return database.accounts.find(
+
+    let account = database.accounts.find(
       acc => acc.id === userId
     );
+
+    if (!account) {
+      account = this.create(userId);
+    }
+
+    return account;
+  },
+
+
+  addMoney(userId, amount) {
+
+    const account = this.get(userId);
+
+    account.money += amount;
+
+    return account.money;
+  },
+
+
+  removeMoney(userId, amount) {
+
+    const account = this.get(userId);
+
+    if (account.money < amount) {
+      return false;
+    }
+
+    account.money -= amount;
+
+    return true;
+  },
+
+
+  transfer(from, to, amount) {
+
+    const sender = this.get(from);
+    const receiver = this.get(to);
+
+
+    if (sender.money < amount) {
+      return false;
+    }
+
+
+    sender.money -= amount;
+    receiver.money += amount;
+
+
+    return true;
   }
+
 };
