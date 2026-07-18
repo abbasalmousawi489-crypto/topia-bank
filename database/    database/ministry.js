7528cf4d64259fa58c6e106e964791079b62ed03
abbasalmousawi489-crypto <abbasalmousawi489@gmail.com>
2026-07-19 00:46:41 +0300

    Update ministry.js

--- database/    database/ministry.js
+++ database/    database/ministry.js
@@ -1,1 +1,17 @@
+const database = require("./database");
 
+module.exports = {
+  add(name, minister) {
+    const ministry = {
+      name: name,
+      minister: minister
+    };
+
+    database.ministries.push(ministry);
+    return ministry;
+  },
+
+  list() {
+    return database.ministries;
+  }
+};
