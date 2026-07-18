6e0e312901d9f44e1ba28b25462e178bc0ae44ce
abbasalmousawi489-crypto <abbasalmousawi489@gmail.com>
2026-07-19 00:55:34 +0300

    Update ministryMenu.js

--- menus/menus/ministryMenu.js
+++ menus/menus/ministryMenu.js
@@ -1,1 +1,30 @@
+module.exports = {
+  name: "ministryMenu",
 
+  options: [
+    {
+      label: "💰 وزارة المالية",
+      value: "finance"
+    },
+    {
+      label: "🛡️ وزارة الدفاع",
+      value: "defense"
+    },
+    {
+      label: "👮 وزارة الداخلية",
+      value: "interior"
+    },
+    {
+      label: "🌍 وزارة الخارجية",
+      value: "foreign"
+    },
+    {
+      label: "📺 وزارة الإعلام",
+      value: "media"
+    },
+    {
+      label: "⚖️ وزارة العدل",
+      value: "justice"
+    }
+  ]
+};
