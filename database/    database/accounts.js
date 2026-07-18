09071ad2f0b97923c396eb03acee97768256a5d8
abbasalmousawi489-crypto <abbasalmousawi489@gmail.com>
2026-07-19 00:46:04 +0300

    Update accounts.js

--- database/    database/accounts.js
+++ database/    database/accounts.js
@@ -1,1 +1,20 @@
+const database = require("./database");
 
+module.exports = {
+  create(userId) {
+    const account = {
+      id: userId,
+      money: 0,
+      card: false
+    };
+
+    database.accounts.push(account);
+    return account;
+  },
+
+  get(userId) {
+    return database.accounts.find(
+      acc => acc.id === userId
+    );
+  }
+};
