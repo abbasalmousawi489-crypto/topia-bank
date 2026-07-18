1b2d13c65e560fa9335e630dd998cd1fa946e829
abbasalmousawi489-crypto <abbasalmousawi489@gmail.com>
2026-07-19 01:59:35 +0300

    Update withdraw.js

--- buttons/buttons/withdraw.js
+++ buttons/buttons/withdraw.js
@@ -1,7 +1,21 @@
-module.exports = {
-  customId: "withdraw",
+const { EmbedBuilder } = require("discord.js");
 
-  async execute(interaction) {
-    await interaction.reply("💵 تم السحب");
-  }
+module.exports = async (interaction) => {
+
+    const embed = new EmbedBuilder()
+        .setTitle("📤 السحب")
+        .setDescription(
+            "💸 يمكنك سحب الأموال من حسابك.\n\n" +
+            "عملة البنك: دينار توبي"
+        )
+        .setFooter({
+            text: "بنك دينار توبي"
+        });
+
+
+    await interaction.reply({
+        embeds: [embed],
+        ephemeral: true
+    });
+
 };
