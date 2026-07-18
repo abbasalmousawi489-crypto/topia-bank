d6175bd5897a843faa338d1f4669d80990314273
abbasalmousawi489-crypto <abbasalmousawi489@gmail.com>
2026-07-19 01:01:00 +0300

    Update advertise.js

--- commands/commands/advertise.js
+++ commands/commands/advertise.js
@@ -1,1 +1,44 @@
+const companies = require("../database/companies");
+const accounts = require("../database/accounts");
 
+
+module.exports = {
+
+  name: "advertise",
+
+  async execute(interaction) {
+
+    const company = interaction.options.getString("company");
+    const text = interaction.options.getString("text");
+    const price = interaction.options.getInteger("price");
+
+
+    const user = accounts.get(interaction.user.id);
+
+
+    if (user.money < price) {
+      return interaction.reply(
+        "❌ لا تملك دينار توبي كافي"
+      );
+    }
+
+
+    accounts.removeMoney(
+      interaction.user.id,
+      price
+    );
+
+
+    companies.addAd(
+      company,
+      text,
+      price
+    );
+
+
+    await interaction.reply(
+      `📢 تم نشر إعلان لشركة **${company}** مقابل ${price} دينار توبي`
+    );
+
+  }
+};
