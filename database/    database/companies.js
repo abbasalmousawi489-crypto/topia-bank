2a818dd7733e6f153ebcd4e6b396aefa0e029c1b
abbasalmousawi489-crypto <abbasalmousawi489@gmail.com>
2026-07-19 00:46:19 +0300

    Update companies.js

--- database/    database/companies.js
+++ database/    database/companies.js
@@ -1,1 +1,17 @@
+const database = require("./database");
 
+module.exports = {
+  add(name, owner) {
+    const company = {
+      name: name,
+      owner: owner
+    };
+
+    database.companies.push(company);
+    return company;
+  },
+
+  list() {
+    return database.companies;
+  }
+};
