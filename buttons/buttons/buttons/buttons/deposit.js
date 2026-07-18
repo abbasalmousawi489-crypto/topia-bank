cd8e6d2ab1253762663df5eaa099b4ddf39305cb
abbasalmousawi489-crypto <abbasalmousawi489@gmail.com>
2026-07-19 01:59:08 +0300

    Update deposit.js

--- buttons/buttons/buttons/buttons/deposit.js
+++ buttons/buttons/buttons/buttons/deposit.js
@@ -1,7 +1,21 @@
-module.exports = {
-  customId: "deposit",
+const { EmbedBuilder } = require("discord.js");
 
-  async execute(interaction) {
-    await interaction.reply("💰 تم الإيداع");
-  }
+module.exports = async (interaction) => {
+
+    const embed = new EmbedBuilder()
+        .setTitle("📥 الإيداع")
+        .setDescription(
+            "💰 يمكنك إضافة أموال إلى حسابك البنكي.\n\n" +
+            "عملة البنك: دينار توبي"
+        )
+        .setFooter({
+            text: "بنك دينار توبي"
+        });
+
+
+    await interaction.reply({
+        embeds: [embed],
+        ephemeral: true
+    });
+
 };
