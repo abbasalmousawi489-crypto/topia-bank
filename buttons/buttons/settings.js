7169d7129dc4ef118382fd1ba0a90a1b7f66b0f0
abbasalmousawi489-crypto <abbasalmousawi489@gmail.com>
2026-07-19 02:01:46 +0300

    Update settings.js

--- buttons/buttons/settings.js
+++ buttons/buttons/settings.js
@@ -1,7 +1,22 @@
-module.exports = {
-  customId: "settings",
+const { EmbedBuilder } = require("discord.js");
 
-  async execute(interaction) {
-    await interaction.reply("⚙️ الإعدادات");
-  }
+module.exports = async (interaction) => {
+
+    const embed = new EmbedBuilder()
+        .setTitle("⚙️ إعدادات الحساب")
+        .setDescription(
+            "يمكنك التحكم بإعدادات حسابك البنكي.\n\n" +
+            "🔐 حماية الحساب\n" +
+            "💳 إعدادات البطاقة\n" +
+            "📜 سجل العمليات"
+        )
+        .setFooter({
+            text: "بنك دينار توبي"
+        });
+
+    await interaction.reply({
+        embeds: [embed],
+        ephemeral: true
+    });
+
 };
