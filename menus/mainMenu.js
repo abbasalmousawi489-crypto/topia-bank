3a23b4ee61960bd1105143e84b89004c6c982f21
abbasalmousawi489-crypto <abbasalmousawi489@gmail.com>
2026-07-19 00:50:48 +0300

    Update mainMenu.js

--- menus/mainMenu.js
+++ menus/mainMenu.js
@@ -1,1 +1,18 @@
+module.exports = {
+  name: "mainMenu",
 
+  options: [
+    {
+      label: "🏦 البنك",
+      value: "bank"
+    },
+    {
+      label: "👤 الحساب",
+      value: "account"
+    },
+    {
+      label: "💸 تحويل",
+      value: "transfer"
+    }
+  ]
+};
