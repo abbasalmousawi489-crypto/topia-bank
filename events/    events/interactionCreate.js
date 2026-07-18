d524220a9ea1495aa7d52f15658ca3e9ba8fa47b
abbasalmousawi489-crypto <abbasalmousawi489@gmail.com>
2026-07-19 00:47:22 +0300

    Update interactionCreate.js

--- events/    events/interactionCreate.js
+++ events/    events/interactionCreate.js
@@ -1,1 +1,21 @@
+module.exports = async (client) => {
 
+  client.on("interactionCreate", async interaction => {
+
+    if (interaction.isButton()) {
+
+      const button = client.buttons.get(interaction.customId);
+
+      if (!button) return;
+
+      try {
+        await button.execute(interaction);
+      } catch (error) {
+        console.log(error);
+      }
+
+    }
+
+  });
+
+};
