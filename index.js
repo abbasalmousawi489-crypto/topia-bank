81e96affe183a008b96f713d42482491dcfe2b87
abbasalmousawi489-crypto <abbasalmousawi489@gmail.com>
2026-07-19 01:01:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,59 @@
 require("dotenv").config();
-
-const { Client, GatewayIntentBits, Events } = require("discord.js");
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const fs = require("fs");
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
   ]
 });
 
-client.once(Events.ClientReady, () => {
-  console.log(`✅ TOPIA BANK جاهز!`);
+
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync("./commands")
+.filter(file => file.endsWith(".js"));
+
+
+for (const file of commandFiles) {
+
+  const command = require(`./commands/${file}`);
+
+  client.commands.set(
+    command.name,
+    command
+  );
+
+}
+
+
+client.once("ready", () => {
+
+  console.log(
+    `${client.user.tag} is online`
+  );
+
 });
+
+
+client.on("interactionCreate", async interaction => {
+
+  if (!interaction.isChatInputCommand()) return;
+
+
+  const command = client.commands.get(
+    interaction.commandName
+  );
+
+
+  if (!command) return;
+
+
+  await command.execute(interaction);
+
+});
+
 
 client.login(process.env.TOKEN);
