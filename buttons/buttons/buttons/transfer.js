b64112889dbd5420befcf0d51f85b7a9420d1dd8
abbasalmousawi489-crypto <abbasalmousawi489@gmail.com>
2026-07-19 01:59:58 +0300

    Update transfer.js

--- buttons/buttons/buttons/transfer.js
+++ buttons/buttons/buttons/transfer.js
@@ -1,7 +1,21 @@
-module.exports = {
-  customId: "transfer",
+const { EmbedBuilder } = require("discord.js");
 
-  async execute(interaction) {
-    await interaction.reply("💸 نظام التحويل");
-  }
+module.exports = async (interaction) => {
+
+    const embed = new EmbedBuilder()
+        .setTitle("💸 تحويل الأموال")
+        .setDescription(
+            "يمكنك تحويل دينار توبي إلى حسابات أخرى.\n\n" +
+            "🔹 نظام التحويل قيد الإعداد."
+        )
+        .setFooter({
+            text: "بنك دينار توبي"
+        });
+
+
+    await interaction.reply({
+        embeds: [embed],
+        ephemeral: true
+    });
+
 };
