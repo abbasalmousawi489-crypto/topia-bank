509611eefdf07a47103fd7c2a8d3ea2da8f306be
abbasalmousawi489-crypto <abbasalmousawi489@gmail.com>
2026-07-19 02:17:16 +0300

    Update interactionCreate.js

--- events/    events/interactionCreate.js
+++ events/    events/interactionCreate.js
@@ -1,60 +1,96 @@
-const بنك = require("../buttons/bank");
-const حساب = require("../buttons/account");
-const تحويل = require("../buttons/transfer");
-const إيداع = require("../buttons/deposit");
-const سحب = require("../buttons/withdraw");
-const وزارة = require("../buttons/ministry");
-const شركة = require("../buttons/company");
-const بطاقة = require("../buttons/card");
-const إعدادات = require("../buttons/settings");
+const bank = require("../buttons/bank");
+const account = require("../buttons/account");
+const transfer = require("../buttons/transfer");
+const deposit = require("../buttons/deposit");
+const withdraw = require("../buttons/withdraw");
+const ministry = require("../buttons/ministry");
+const company = require("../buttons/company");
+const card = require("../buttons/card");
+const settings = require("../buttons/settings");
+const history = require("../buttons/history");
 
-const القائمة_الرئيسية = require("../menus/mainMenu");
-const قائمة_الوزارات = require("../menus/ministryMenu");
-const قائمة_الشركات = require("../menus/companyMenu");
+
+const mainMenu = require("../menus/mainMenu");
+const ministryMenu = require("../menus/ministryMenu");
+const companyMenu = require("../menus/companyMenu");
+
 
 module.exports = async (interaction) => {
 
+
     if (interaction.isButton()) {
 
-        if (interaction.customId === "bank")
-            return بنك(interaction);
 
-        if (interaction.customId === "account")
-            return حساب(interaction);
+        switch (interaction.customId) {
 
-        if (interaction.customId === "transfer")
-            return تحويل(interaction);
 
-        if (interaction.customId === "deposit")
-            return إيداع(interaction);
+            case "bank":
+                return bank(interaction);
 
-        if (interaction.customId === "withdraw")
-            return سحب(interaction);
 
-        if (interaction.customId === "ministry")
-            return وزارة(interaction);
+            case "account":
+                return account(interaction);
 
-        if (interaction.customId === "company")
-            return شركة(interaction);
 
-        if (interaction.customId === "card")
-            return بطاقة(interaction);
+            case "transfer":
+                return transfer(interaction);
 
-        if (interaction.customId === "settings")
-            return إعدادات(interaction);
+
+            case "deposit":
+                return deposit(interaction);
+
+
+            case "withdraw":
+                return withdraw(interaction);
+
+
+            case "card":
+                return card(interaction);
+
+
+            case "history":
+                return history(interaction);
+
+
+            case "settings":
+                return settings(interaction);
+
+
+            case "ministry":
+                return ministry(interaction);
+
+
+            case "company":
+                return company(interaction);
+
+
+        }
+
     }
+
 
 
     if (interaction.isStringSelectMenu()) {
 
-        if (interaction.customId === "main_menu")
-            return القائمة_الرئيسية(interaction);
 
-        if (interaction.customId === "ministry_menu")
-            return قائمة_الوزارات(interaction);
+        switch (interaction.customId) {
 
-        if (interaction.customId === "company_menu")
-            return قائمة_الشركات(interaction);
+
+            case "main_menu":
+                return mainMenu(interaction);
+
+
+            case "ministry_menu":
+                return ministryMenu(interaction);
+
+
+            case "company_menu":
+                return companyMenu(interaction);
+
+
+        }
+
     }
+
 
 };
