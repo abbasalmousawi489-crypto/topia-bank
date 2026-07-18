c58ac7225d761fe940bae4f7696070573d74c490
abbasalmousawi489-crypto <abbasalmousawi489@gmail.com>
2026-07-19 00:56:07 +0300

    Update companyMenu.js

--- menus/menus/menus/companyMenu.js
+++ menus/menus/menus/companyMenu.js
@@ -1,1 +1,34 @@
+module.exports = {
+  name: "companyMenu",
 
+  options: [
+    {
+      label: "🍎 Apple",
+      value: "apple"
+    },
+    {
+      label: "📱 Samsung",
+      value: "samsung"
+    },
+    {
+      label: "💻 Microsoft",
+      value: "microsoft"
+    },
+    {
+      label: "🔎 Google",
+      value: "google"
+    },
+    {
+      label: "🚗 Tesla",
+      value: "tesla"
+    },
+    {
+      label: "📦 Amazon",
+      value: "amazon"
+    },
+    {
+      label: "🎮 Sony",
+      value: "sony"
+    }
+  ]
+};
