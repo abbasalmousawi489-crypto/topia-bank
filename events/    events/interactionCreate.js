ead1eaf6f336ebd204b9aca7f717b3547b585d5a
abbasalmousawi489-crypto <abbasalmousawi489@gmail.com>
2026-07-19 01:56:48 +0300

    Update interactionCreate.js

--- events/    events/interactionCreate.js
+++ events/    events/interactionCreate.js
@@ -1,21 +1,60 @@
-module.exports = async (client) => {
+const بنك = require("../buttons/bank");
+const حساب = require("../buttons/account");
+const تحويل = require("../buttons/transfer");
+const إيداع = require("../buttons/deposit");
+const سحب = require("../buttons/withdraw");
+const وزارة = require("../buttons/ministry");
+const شركة = require("../buttons/company");
+const بطاقة = require("../buttons/card");
+const إعدادات = require("../buttons/settings");
 
-  client.on("interactionCreate", async interaction => {
+const القائمة_الرئيسية = require("../menus/mainMenu");
+const قائمة_الوزارات = require("../menus/ministryMenu");
+const قائمة_الشركات = require("../menus/companyMenu");
+
+module.exports = async (interaction) => {
 
     if (interaction.isButton()) {
 
-      const button = client.buttons.get(interaction.customId);
+        if (interaction.customId === "bank")
+            return بنك(interaction);
 
-      if (!button) return;
+        if (interaction.customId === "account")
+            return حساب(interaction);
 
-      try {
-        await button.execute(interaction);
-      } catch (error) {
-        console.log(error);
-      }
+        if (interaction.customId === "transfer")
+            return تحويل(interaction);
 
+        if (interaction.customId === "deposit")
+            return إيداع(interaction);
+
+        if (interaction.customId === "withdraw")
+            return سحب(interaction);
+
+        if (interaction.customId === "ministry")
+            return وزارة(interaction);
+
+        if (interaction.customId === "company")
+            return شركة(interaction);
+
+        if (interaction.customId === "card")
+            return بطاقة(interaction);
+
+        if (interaction.customId === "settings")
+            return إعدادات(interaction);
     }
 
-  });
+
+    if (interaction.isStringSelectMenu()) {
+
+        if (interaction.customId === "main_menu")
+            return القائمة_الرئيسية(interaction);
+
+        if (interaction.customId === "ministry_menu")
+            return قائمة_الوزارات(interaction);
+
+        if (interaction.customId === "company_menu")
+            return قائمة_الشركات(interaction);
+    }
 
 };
