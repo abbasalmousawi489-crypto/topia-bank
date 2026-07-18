401fd04aa2ec169175824af3d5816e66ab8e78e2
abbasalmousawi489-crypto <abbasalmousawi489@gmail.com>
2026-07-19 02:11:10 +0300

    Update account.js

--- buttons/buttons/account.js
+++ buttons/buttons/account.js
@@ -1,47 +1,28 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
-
-const الحسابات = require("../database/accounts");
+const { EmbedBuilder } = require("discord.js");
+const database = require("../database/database");
 
 module.exports = async (interaction) => {
 
     const id = interaction.user.id;
 
-    let حساب = الحسابات[id];
 
-    if (!حساب) {
-        حسابات[id] = {
-            الرصيد: 1000,
-            العملة: "دينار توبي"
-        };
-
-        حساب = حسابات[id];
-    }
+    const حساب = database.جلب_بيانات_المستخدم(id);
 
 
-    const رسالة = new EmbedBuilder()
+    const embed = new EmbedBuilder()
         .setTitle("💳 الحساب البنكي")
         .setDescription(
             `👤 صاحب الحساب: ${interaction.user}\n\n` +
-            `💰 الرصيد: ${حساب.الرصيد} ${حساب.العملة}\n\n` +
-            `🏦 حالة الحساب: نشط`
+            `💰 الرصيد: ${حساب.الرصيد} دينار توبي (TND)\n\n` +
+            `🏦 حالة الحساب: فعال`
         )
         .setFooter({
             text: "بنك دينار توبي المركزي"
         });
 
 
-    const زر = new ActionRowBuilder()
-        .addComponents(
-            new ButtonBuilder()
-                .setCustomId("card")
-                .setLabel("💳 البطاقة البنكية")
-                .setStyle(ButtonStyle.Primary)
-        );
-
-
     await interaction.reply({
-        embeds: [رسالة],
-        components: [زر],
+        embeds: [embed],
         ephemeral: true
     });
 
